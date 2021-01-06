3ba2eaeff1939d6be7b8899ef4fcaff5262e908a
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-05 22:28:08 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let messages = [];
+    for(let i=0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages
+}
+
+function countDown(number) {
+    let i = number;
+
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+    // for(let i = number; i >= 0; i--){
+    //     console.log(i)
+    // }
+}
